207fdd4ed402c4d645f19b5dd6e5a025b7eeeca3
William <willspri@gmail.com>
2024-05-04 08:38:39 +1200

    committed

---
+++ MemoryUsage.ps1
@@ -0,0 +1,10 @@
+#get top ten processes using most memory
+$topProcesses= Get-Process | Sort-Object -Property WorkingSet -Descending | Select-Object -First 20
+
+#result output
+Write-Host "Top 20 Processes: "
+foreach ($process in $topProcesses) {
+	$processName = $process.ProcessName
+	$memoryUsage = $process.WorkingSet / 1MB # Convert memory usage to MB
+	Write-Host "$processName - Memory Usage: $memoryUsage MB"
+}
